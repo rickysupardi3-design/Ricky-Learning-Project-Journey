bf54091ea107bcef61f0d0ecb37ef8b4036e5c82
Your Name <your.email@example.com>
2026-09-13 18:59:08 +0700

    Add image only to post id 2

--- src/data/posts.js
+++ src/data/posts.js
@@ -8,9 +8,10 @@ export const mockPosts = [
   },
   {
     id: 2,
-    title: "Python Panda Library",
+    title: "Episode 1 : Learning Python",
     date: "September 13, 2026",
     excerpt: "Day 1 Goal : Extract data from excel files using Python",
     content: "At first I import panda library and read the excel file using pandas.read_excel() function. Hope can manipulate the data using pandas DataFrame methods. But it;s fail and only show result NaN (Not a Number).",
+    image: "/screenshot1.png"
   }
 ];
